898009c2a8e742b942621aeb44285bc18fe05e4c
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-03-17 14:36:10 +0900

    episode 25

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'));
+
+Route::get('register', [RegisteredUserController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisteredUserController::class, 'store'])->middleware('guest');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
