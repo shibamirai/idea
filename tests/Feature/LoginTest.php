5f55c3463ce0e820c89f1abdef4e2ade3b441903
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-03-17 16:56:16 +0900

    episode 26

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertAuthenticated;
+use function Pest\Laravel\assertGuest;
+
+it('logs in a user', function () {
+    $user = User::factory()->create(['password' => 'password123!@#']);
+
+    visit('/login')
+        ->fill('email', $user->email)
+        ->fill('password', 'password123!@#')
+        ->click('@login-button')
+        ->assertPathIs('/');
+
+    assertAuthenticated();
+});
+
+it('logs out a user', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    visit('/')->click('Log Out');
+
+    assertGuest();
+});
---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use function Pest\Laravel\assertAuthenticated;
+
+it('registers a user', function () {
+    visit('/register')
+        ->fill('name', 'John Doe')
+        ->fill('email', 'john@example.com')
+        ->fill('password', 'password123!@#')
+        ->click('Create Account')
+        ->assertPathIs('/');
+
+    assertAuthenticated();
+
+    expect(Auth::user())->toMatchArray([
+        'name' => 'John Doe',
+        'email' => 'john@example.com',
+    ]);
+});
+
+it('requires a valid email', function () {
+    visit('/register')
+        ->fill('name', 'John Doe')
+        ->fill('email', 'john123')
+        ->fill('password', 'password123!@#')
+        ->click('Create Account')
+        ->assertPathIs('/register');
+
+});
