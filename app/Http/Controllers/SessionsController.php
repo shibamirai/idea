bcbb90be75593f1a085ea7ceabc5c099e3c0cf99
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-03-18 09:58:41 +0900

    amend

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,9 +31,12 @@ class SessionsController extends Controller
         return redirect()->intended('/')->with('success', 'You are now logged in.');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
         <div>
             <a href="/">
-                <img src="/images/logo.png" width="100" alt="Idea logo">
+                <img src="/images/logo.png" width="100" height="auto" alt="Idea logo">
             </a>
         </div>
 
