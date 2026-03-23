3a828fce7cad6bbb89d9db07dede8832b7798762
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-03-23 13:59:19 +0900

    Add support for Markdonw formatting

--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -20,3 +20,9 @@ test('it can have steps', function () {
 
     expect($idea->fresh()->steps)->toHaveCount(1);
 });
+
+test('it can format a description using Markdown', function () {
+    $idea = new Idea(['description' => 'Hello *world*']);
+
+    expect($idea->formattedDescription)->toEqual("<p>Hello <em>world</em></p>\n");
+});
